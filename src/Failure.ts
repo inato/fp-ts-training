8e466d82f3e8307d6b605a1295e7da4204a995c9
Hugo Saracino <hugo@inato.com>
2020-07-06 15:18:39 +0200

    🔧 Add utilitary Failure type

---
+++ src/Failure.ts
@@ -0,0 +1,89 @@
+import * as decod from 'decod';
+import * as Either from 'fp-ts/lib/Either';
+import * as TaskEither from 'fp-ts/lib/TaskEither';
+import * as Task from 'fp-ts/lib/Task';
+import { pipe } from 'fp-ts/lib/pipeable';
+
+const decodeErrorMessage = decod.at('message', decod.string);
+
+export enum FailureType {
+  Unexpected = 'FailureType_Unexpected',
+}
+
+export type UnexpectedFailure = Failure<FailureType.Unexpected>;
+
+export class Failure<T extends string = FailureType> {
+  constructor({
+    reason,
+    type,
+    originalError,
+  }: {
+    reason: string;
+    type: T;
+    originalError?: Error | unknown;
+  }) {
+    this.reason = reason;
+    this.type = type;
+    if (originalError instanceof Error) {
+      this.originalError = originalError;
+    }
+  }
+
+  reason: string;
+
+  type: T;
+
+  originalError?: Error;
+
+  toError(): Error {
+    if (this.originalError) return this.originalError;
+    return new Error(this.reason);
+  }
+
+  static builder<T extends string>(type: T) {
+    return (reason: string, originalError?: Error | unknown) =>
+      new Failure<T>({
+        reason,
+        type,
+        originalError,
+      });
+  }
+
+  static unexpected = Failure.builder(FailureType.Unexpected);
+
+  static fromUnknownError(originalError: unknown | Error): UnexpectedFailure {
+    return Failure.unexpected(decodeErrorMessage(originalError), originalError);
+  }
+
+  static toType<T extends string>(type: T): (failure: Failure) => Failure<T> {
+    return (failure: Failure) =>
+      new Failure({
+        reason: failure.reason,
+        type,
+        originalError: failure.originalError,
+      });
+  }
+
+  static toUnexpected = Failure.toType(FailureType.Unexpected);
+
+  private static throw = (failure: Failure) => {
+    throw failure.toError();
+  };
+
+  static eitherUnsafeGet = <A>(either: Either.Either<Failure, A>): A =>
+    pipe(either, Either.getOrElseW(Failure.throw));
+
+  static taskEitherUnsafeGet = <A>(
+    taskEither: TaskEither.TaskEither<Failure, A>,
+  ): Task.Task<A> =>
+    pipe(
+      taskEither,
+      TaskEither.getOrElseW((failure) => async () => Failure.throw(failure)),
+    );
+}
+
+export type FailureTypes<F extends Record<string, string>> = F[keyof F];
+export interface FailureTypings<F extends Record<string, string>> {
+  types: FailureTypes<F>;
+  failure: Failure<FailureTypes<F>>;
+}
