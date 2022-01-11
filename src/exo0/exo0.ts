ab257b0d383d8d727debe37ba335d5c0293d4064
Hugo Saracino <hugo@inato.com>
2022-01-11 15:24:41 +0100

    ♻️ Solution for exo 0

--- src/exo0/exo0.ts
+++ src/exo0/exo0.ts
@@ -24,7 +24,7 @@
 // - `flow(f) === f`
 // - `pipe(x, f) === f(x)`
 
-import { unimplemented } from '../utils';
+import { flow, pipe } from 'fp-ts/function';
 
 export const isEven = (value: number) => value % 2 === 0;
 
@@ -42,9 +42,9 @@ export const not = (value: boolean) => !value;
 // `pipe` or `flow`), write the function `isOdd` that checks if a number is
 // odd.
 
-export const isOddP: (value: number) => boolean = unimplemented;
+export const isOddP = (value: number) => pipe(value, isEven, not);
 
-export const isOddF: (value: number) => boolean = unimplemented;
+export const isOddF = flow(isEven, not);
 
 // We will write a function that for any given number, computes the next
 // one according to the following rules:
@@ -63,10 +63,18 @@ export const ifThenElse =
 // Using `pipe` and `ifThenElse`, write the function that computes the next step in the Collatz
 // sequence.
 
-export const next: (value: number) => number = unimplemented;
+export const next = (value: number) =>
+  pipe(
+    value,
+    isEven,
+    ifThenElse(
+      () => value / 2,
+      () => value * 3 + 1,
+    ),
+  );
 
 // Using only `flow` and `next`, write the function that for any given number
 // a_n from the Collatz sequence, returns the number a_n+3 (ie. the number
 // three steps ahead in the sequence).
 
-export const next3: (value: number) => number = unimplemented;
+export const next3 = flow(next, next, next);
