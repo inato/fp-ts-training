30e48252f806b12b104356182628ffd8103e5dcf
Hugo Saracino <hugo@inato.com>
2022-01-12 14:21:03 +0100

    ♻️ Solution for exo 4

--- src/exo4/exo4.ts
+++ src/exo4/exo4.ts
@@ -1,9 +1,9 @@
 // `fp-ts` training Exercise 4
 // Dependency injection with `Reader`
 
+import { reader } from 'fp-ts';
+import { flow, pipe } from 'fp-ts/function';
 import { Reader } from 'fp-ts/Reader';
-
-import { unimplemented } from '../utils';
 
 // Sometimes, a function can have a huge amount of dependencies (services,
 // repositories, ...) and it is often impractical (not to say truly annoying)
@@ -44,8 +44,22 @@ export enum Country {
 //
 // HINT: Take a look at `reader.ask` to access the environment value
 
-export const exclamation: (sentence: string) => Reader<Country, string> =
-  unimplemented();
+export const exclamation = (sentence: string): Reader<Country, string> =>
+  pipe(
+    reader.ask<Country>(),
+    reader.map((country) => {
+      switch (country) {
+        case Country.France:
+          return `${sentence} !`;
+
+        case Country.Spain:
+          return `¡${sentence}!`;
+
+        case Country.USA:
+          return `${sentence}!`;
+      }
+    }),
+  );
 
 // Obviously, different countries often mean different languages and so
 // different words for saying "Hello":
@@ -70,7 +84,11 @@ export const sayHello = (country: Country): string => {
 // HINT: You can look into `reader.map` to modify the output of a `Reader`
 // action.
 
-export const greet: (name: string) => Reader<Country, string> = unimplemented();
+export const greet = (name: string): Reader<Country, string> =>
+  pipe(
+    sayHello,
+    reader.map((greeting) => `${greeting}, ${name}`),
+  );
 
 // Finally, we are going to compose multiple `Reader`s together.
 //
@@ -84,5 +102,4 @@ export const greet: (name: string) => Reader<Country, string> = unimplemented();
 // HINT: As with other wrapper types in `fp-ts`, `reader` offers a way of
 // composing effects with `reader.chain`.
 
-export const excitedlyGreet: (name: string) => Reader<Country, string> =
-  unimplemented();
+export const excitedlyGreet = flow(greet, reader.chain(exclamation));
