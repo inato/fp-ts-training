b69c467174485d5633aa4614bf9663d4282401f3
Justine Mignot <mignot.justine@gmail.com>
2022-05-13 10:15:21 +0200

    :sparkles: add bonus (about if) in exo1

--- src/exo1/exo1.ts
+++ src/exo1/exo1.ts
@@ -51,6 +51,17 @@ export const safeDivideWithError: (
   b: number,
 ) => Either<DivisionByZeroError, number> = unimplemented;
 
+
+// COMMENT ON BONUS: 
+// You can see here we combined not functional programming (safeDivide written with if) 
+// and functional programming (using safeDivide in a pipe)
+// Try to re-write safeDivideWithError with only functional programming now (no if)
+// And check test are still passing
+// 
+// HINT: have a look at either `fromPredicate` constructor
+
+
+
 ///////////////////////////////////////////////////////////////////////////////
 //                                TASKEITHER                                 //
 ///////////////////////////////////////////////////////////////////////////////
