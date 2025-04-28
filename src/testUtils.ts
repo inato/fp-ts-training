cd42c8e694035ef258a8e0ba2b57d0d45cbb118e
Kevin T. <kevin.trancong@yahoo.fr>
2025-04-28 09:15:27 +0200

    Add missing testUtils update

--- src/testUtils.ts
+++ src/testUtils.ts
@@ -1,1 +1,13 @@
-export const isTestingSolution = () => process.env.TEST_SOLUTION === 'true';
+// Usually during training, trainees import both their implementation and the solution
+// but only use one at a time to check their results.
+// The function `isTestingSolution()` allows testing to toggle between the two
+// by using the environment variable `SOLUTION`.
+
+export function isTestingSolution() {
+  return process.env.SOLUTION === '1' || process.env.SOLUTION === 'true'
+}
+
+// For testing solution only
+export function testSolution() {
+  return true
+}
