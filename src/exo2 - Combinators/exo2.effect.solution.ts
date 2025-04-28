1969054608ca0f3b62bda3c93ff3bf20566fcef9
Kevin T. <kevin.trancong@yahoo.fr>
2025-04-28 09:02:07 +0200

    Update exo2 to Effect

---
+++ src/exo2 - Combinators/exo2.effect.solution.ts
@@ -0,0 +1,198 @@
+// Effect training Exercise 2
+// Let's have fun with combinators!
+
+import { Option, Either } from "effect";
+import { Failure } from '../Failure';
+
+///////////////////////////////////////////////////////////////////////////////
+//                                   SETUP                                   //
+///////////////////////////////////////////////////////////////////////////////
+
+// We are developing a small game, and the player can control either one of
+// three types of characters, mainly differentiated by the type of damage they
+// can put out.
+
+// Our main `Character` type is a simple union of all the concrete character
+// types.
+export type Character = Warrior | Wizard | Archer;
+
+// We have three types of `Damage`, each corresponding to a character type.
+export enum Damage {
+  Physical = 'Physical damage',
+  Magical = 'Magical damage',
+  Ranged = 'Ranged damage',
+}
+
+// A `Warrior` only can output physical damage.
+export class Warrior {
+  smash() {
+    return Damage.Physical;
+  }
+
+  toString() {
+    return 'Warrior';
+  }
+}
+
+// A `Wizard` only can output magical damage.
+export class Wizard {
+  burn() {
+    return Damage.Magical;
+  }
+
+  toString() {
+    return 'Wizard';
+  }
+}
+
+// An `Archer` only can output ranged damage.
+export class Archer {
+  shoot() {
+    return Damage.Ranged;
+  }
+
+  toString() {
+    return 'Archer';
+  }
+}
+
+// We also have convenient type guards to help us differentiate between
+// character types when given a `Character`.
+
+export const isWarrior = (character: Character): character is Warrior => {
+  return (character as Warrior).smash !== undefined;
+};
+
+export const isWizard = (character: Character): character is Wizard => {
+  return (character as Wizard).burn !== undefined;
+};
+
+export const isArcher = (character: Character): character is Archer => {
+  return (character as Archer).shoot !== undefined;
+};
+
+// Finally, we have convenient and expressive error types, defining what can
+// go wrong in our game:
+// - the player can try to perform an action without first choosing a character as the attacker
+// - the player can try to perform the wrong action for a character class
+
+export enum Exo2FailureType {
+  NoAttacker = 'Exo2FailureType_NoAttacker',
+  InvalidAttacker = 'Exo2FailureType_InvalidAttacker',
+}
+
+export type NoAttackerFailure = Failure<Exo2FailureType.NoAttacker>;
+export const noAttackerFailure = Failure.builder(Exo2FailureType.NoAttacker);
+
+export type InvalidAttackerFailure = Failure<Exo2FailureType.InvalidAttacker>;
+export const invalidAttackerFailure = Failure.builder(
+  Exo2FailureType.InvalidAttacker,
+);
+
+///////////////////////////////////////////////////////////////////////////////
+//                                  EITHER                                   //
+///////////////////////////////////////////////////////////////////////////////
+
+// Helper functions for checking character types
+// Note: In Effect, Either is Either<Right, Left> (opposite to fp-ts)
+export const checkAttackerAndSmash = (attacker: Option.Option<Character>): Either.Either<Damage, NoAttackerFailure | InvalidAttackerFailure> => {
+  if (Option.isNone(attacker)) {
+    return Either.left(noAttackerFailure('No attacker currently selected'));
+  }
+  
+  const character = Option.getOrUndefined(attacker)!;
+  
+  if (!isWarrior(character)) {
+    return Either.left(invalidAttackerFailure(`${character.toString()} cannot perform smash`));
+  }
+  
+  return Either.right(character.smash());
+};
+
+export const checkAttackerAndBurn = (attacker: Option.Option<Character>): Either.Either<Damage, NoAttackerFailure | InvalidAttackerFailure> => {
+  if (Option.isNone(attacker)) {
+    return Either.left(noAttackerFailure('No attacker currently selected'));
+  }
+  
+  const character = Option.getOrUndefined(attacker)!;
+  
+  if (!isWizard(character)) {
+    return Either.left(invalidAttackerFailure(`${character.toString()} cannot perform burn`));
+  }
+  
+  return Either.right(character.burn());
+};
+
+export const checkAttackerAndShoot = (attacker: Option.Option<Character>): Either.Either<Damage, NoAttackerFailure | InvalidAttackerFailure> => {
+  if (Option.isNone(attacker)) {
+    return Either.left(noAttackerFailure('No attacker currently selected'));
+  }
+  
+  const character = Option.getOrUndefined(attacker)!;
+  
+  if (!isArcher(character)) {
+    return Either.left(invalidAttackerFailure(`${character.toString()} cannot perform shoot`));
+  }
+  
+  return Either.right(character.shoot());
+};
+
+///////////////////////////////////////////////////////////////////////////////
+//                                  OPTION                                   //
+///////////////////////////////////////////////////////////////////////////////
+
+// Convert Either-based functions to Option-based ones
+export const smashOption = (character: Character): Option.Option<Damage> => {
+  if (isWarrior(character)) {
+    return Option.some(character.smash());
+  }
+  return Option.none();
+};
+
+export const burnOption = (character: Character): Option.Option<Damage> => {
+  if (isWizard(character)) {
+    return Option.some(character.burn());
+  }
+  return Option.none();
+};
+
+export const shootOption = (character: Character): Option.Option<Damage> => {
+  if (isArcher(character)) {
+    return Option.some(character.shoot());
+  }
+  return Option.none();
+};
+
+///////////////////////////////////////////////////////////////////////////////
+//                                   ARRAY                                   //
+///////////////////////////////////////////////////////////////////////////////
+
+export interface TotalDamage {
+  [Damage.Physical]: number;
+  [Damage.Magical]: number;
+  [Damage.Ranged]: number;
+}
+
+export const attack = (army: ReadonlyArray<Character>): TotalDamage => {
+  let physical = 0;
+  let magical = 0;
+  let ranged = 0;
+  
+  for (const character of army) {
+    if (Option.isSome(smashOption(character))) {
+      physical++;
+    }
+    if (Option.isSome(burnOption(character))) {
+      magical++;
+    }
+    if (Option.isSome(shootOption(character))) {
+      ranged++;
+    }
+  }
+  
+  return {
+    [Damage.Physical]: physical,
+    [Damage.Magical]: magical,
+    [Damage.Ranged]: ranged
+  };
+}; 
